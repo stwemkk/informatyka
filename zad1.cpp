c2e10e3aa4acdecd1d17b320d1bc3bf86246cc8c
Emil Krawczyk <mail.emilkrawczyk@gmail.com>
2021-09-21 21:13:58 +0200

    Zadania

---
+++ zad1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=10; i<100; i=i+2)
+        cout << i << " ";
+    return 0;
+}
---
+++ zad2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=999; i>=777; i=i-3)
+        cout << i << " ";
+    return 0;
+}
