1a6c345b34ec311e3a2691b35c289ba9ecaeb48f
Emil Krawczyk <mail.emilkrawczyk@gmail.com>
2021-10-13 10:28:09 +0200

    Trójkąt z gwiazdek - 13.10.2021

---
+++ trojkat.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+     for(int i=0; i<=7; i++){
+
+        for(int l=1; l<=i; l++){
+
+            cout << "*";
+
+
+        }
+
+            cout << endl;
+
+     }
+}
+
+
+/*/
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+* * * * * * *
+
+/*/
